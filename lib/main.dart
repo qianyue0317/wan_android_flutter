9227d0cbf1a3dbc267451d78841941986bed8c7e
QY <1974765766@qq.com>
2023-09-21 23:30:58 +0800

    添加异常处理

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
 }
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -32,42 +33,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   int _selectedItemIndex = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "第一"),
-        BottomNavigationBarItem(icon: Icon(Icons.business), label: "第二"),
-        BottomNavigationBarItem(icon: Icon(Icons.school), label: "第三")
-      ], currentIndex: _selectedItemIndex,
-      onTap: _onNavItemTapped,),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "第一"),
+          BottomNavigationBarItem(icon: Icon(Icons.business), label: "第二"),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: "第三")
+        ],
+        currentIndex: _selectedItemIndex,
+        onTap: _onNavItemTapped,
       ),
     );
   }
