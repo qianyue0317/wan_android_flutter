0e26d25e6b91658071a5e720af8959a5bd43b6a6
QY <1974765766@qq.com>
2023-10-10 17:01:36 +0800

    我的页面简单修改

--- lib/tabpage/mine_page.dart
+++ lib/tabpage/mine_page.dart
@@ -1,5 +1,4 @@
-
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:wan_android_flutter/base/base_page.dart';
 
 class MinePage extends StatefulWidget {
@@ -9,17 +8,48 @@ class MinePage extends StatefulWidget {
   State<StatefulWidget> createState() => _MinePageState();
 }
 
-
-class _MinePageState extends State<MinePage> with BasePage<MinePage>, AutomaticKeepAliveClientMixin {
+class _MinePageState extends State<MinePage>
+    with BasePage<MinePage>, AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return const Center(
-      child: Text("mine"),
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Image.asset("assets/images/icon_collect.png",
+                        width: 48, height: 48),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
+                      child: const Text("用户名"),
+                    ),
+                    const Expanded(
+                        child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text("积分： 1091"),
+                    ))
+                  ],
+                )),
+            Expanded(
+                child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(24)),
+              ),
+            ))
+          ],
+        ),
+      ),
     );
   }
 
   @override
   bool get wantKeepAlive => true;
-
 }
